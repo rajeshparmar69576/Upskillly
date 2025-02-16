c2fbbe29c84752a9f53edf16763922faf745361c
rajeshparmar69576 <rp69576@gmail.com>
2025-02-16 11:35:59 +0530

    configured cloudinary

---
+++ server/config/cloudinary.config.js
@@ -0,0 +1,16 @@
+const cloudinary = require('cloudinary').v2
+
+exports.clodinaryConnect = () => {
+    try{
+        cloudinary.config({
+            // ############ Configuring clodinary to upload media ############
+            cloud_name:process.env.CLOUD_NAME,
+            api_key:process.env.API_KEY,
+            api_secret:process.env.API_SECRET
+        })
+
+    }catch(error){
+        console.log(error)
+    }
+
+};
