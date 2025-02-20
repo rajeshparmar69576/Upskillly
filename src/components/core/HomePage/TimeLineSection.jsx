0d5fa5b56d0f411ba8ff97330d08fadb96b4bb80
rajeshparmar69576 <rp69576@gmail.com>
2025-02-20 23:58:13 +0530

    Created section 2 for home Page

---
+++ src/components/core/HomePage/TimeLineSection.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const timeline = [
+    {
+        Logo: Logo1,
+        heading: 'LeaderShip'
+    }
+]
+
+const TimeLineSection = () => {
+  return (
+    <div>
+        <div className='flex flex-row gap-15 items-center'>
+
+            <div className='w-[45px] flex flex-col gap-5'>
+
+            </div> 
+
+        </div>
+    </div>
+  )
+}
+
+export default TimeLineSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import HighlightText from '../components/core/HomePage/HighlightText';
 import Button from '../components/core/HomePage/Button';
 import Banner from '../assets/Images/banner.mp4'
 import CodeBlocks from '../components/core/HomePage/CodeBlocks';
+import TimeLineSection from '../components/core/HomePage/TimeLineSection';
+import LearningLanguageSection from '../components/core/HomePage/LearningLanguageSection';
 
 const Home = () => {
   return (
@@ -136,6 +138,49 @@ const Home = () => {
         </div>
 
         {/* Section 2 */}
+        <div className='bg-[#f9f9f9] text-richblack-700'>
+            <div className='homepage_bg h-[320px]'>
+                <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto'>
+                    <div className='h-[150px]'></div>
+                    <div className='flex flex-row gap-7 text-white'>
+                        <Button active={true} linkto={'/signup'}>
+                            <div className='flex items-center gap-3'>
+                                Explore full catalog
+                                <FaArrowRight/>
+                            </div>
+                        </Button>
+
+                        <Button active={false} linkto={'/signup'}>
+                            <div>
+                                Learn More
+                            </div>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+
+            <div className='mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7 '>
+                 <div className='flex flex-row gap-5 mb-10 mt-[95px]'>
+                    <div className='text-4xl font-semibold w-[45%]'>
+                        Get the skills you need for a 
+                        <HighlightText text={"Job that is in demand"}/>
+                    </div>
+                    <div className='flex flex-col gap-10 w-[40%] items-start'>
+                        <div className='text-[16px]'>
+                            The modern StudyNotion is the dictates its own terms. Today, to
+                            be a competitive specialist requires more than professional
+                            skills.
+                        </div>
+                        <Button active={true} linkto={'/signup'}>
+                            Learn more
+                        </Button>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
 
 
         {/* Section 3 */}
